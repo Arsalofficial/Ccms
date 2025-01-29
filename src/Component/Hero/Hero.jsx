5a4c68f222af34773c9d1e09a46190eb52cf8293
Arsalofficial <arsalzaheer430@gmail.com>
2025-01-29 16:06:49 +0500

    Resolved merge conflicts

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
         </div>
 
         {/* Job Options */}
-        <div className="mt-6 bg-[#F0F0F0] p-4 rounded-lg shadow-inner w-full sm:w-2/3 lg:w-1/2 ">
+        <div className="mt-6 bg-[#F0F0F0] p-4 rounded-lg shadow-inner w-full sm:w-2/3 lg:w-1/2">
           <form className="space-y-6 mb-8 mt-6 font-semibold px-4">
             {/* PHP Laravel Developer */}
             <label className="flex items-center">
